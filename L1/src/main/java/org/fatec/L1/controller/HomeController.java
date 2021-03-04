7ddd5e9ca7dbb2337e19e502e8065d89c755731f
MARCELOUCHOASDEOLIVE <MARCELOUCHOASDEOLIVE@DESKTOP-PM7M9J5>
2021-03-04 10:43:33 -0300

    Iniciando a lista L1.

---
+++ L1/src/main/java/org/fatec/L1/controller/HomeController.java
@@ -0,0 +1,17 @@
+package org.fatec.L1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController {
+	
+	@GetMapping("/")
+	public String paginaInicial() {
+		return "index";
+		
+	}
+
+}
