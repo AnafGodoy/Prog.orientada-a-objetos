6bc91b18b1a5f743c9efaca084a867341086de6e
AnafGodoy <anaclaragodoy01@gmail.com>
2021-03-31 19:54:34 -0300

    pagina homecontroller

---
+++ Projeto-POO/src/main/java/POO3/Semestre/Projeto/POO/ProjetoPooApplication.java
@@ -0,0 +1,13 @@
+package POO3.Semestre.Projeto.POO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPooApplication.class, args);
+	}
+
+}
